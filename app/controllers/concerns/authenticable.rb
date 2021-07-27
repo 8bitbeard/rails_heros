0af58be4edd93fc6006d42cad408180aeff17ae1
Wilton Souza <willsouzafilho@gmail.com>
2021-07-27 11:28:42 -0300

    Change CRUD to use DB token

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -5,11 +5,11 @@ module Authenticable
     @token = request.headers['Authorization']
 
     unless valid_token?
-      render json: { errors: "You don't have access to this operation" }, status: :unauthorized
+      render json: { errors: "Please inform a authorization header to authenticate (any with at least 10 chars)" }, status: :unauthorized
     end
   end
 
   def valid_token?
-    @token.present? && @token == Rails.application.credentials.token
+    @token.present? && @token.size >= 10
   end
 end
